c2d718feccc46676cd1d0844e2ef2521441f8eaa
sirujam <siruja.maharjan@gmail.com>
2015-02-06 22:35:59 +0545

    Added transparent background

--- plot3.R
+++ plot3.R
@@ -4,6 +4,8 @@ fileErrPrmpt <- "Please copy the file to the directory and/or download file from
 wd <- getwd()
 
 ## Check if the file exists
+## Since the file is big(126 MB), script does not try to download but outputs link for the manual download
+## If required download.file() can be used
 if(!file.exists(fileName)){
   stop(paste(c("\nFile does not exists in ", wd, " .\n", fileErrPrmpt)))
 }
@@ -15,7 +17,8 @@ dataset <- subset(raw_data, regexpr("^0?(1|2)/0?2/2007", raw_data$Date)>0)
 datetime <- strptime(paste(dataset$Date,dataset$Time), "%d/%m/%Y %H:%M:%S", tz='UTC')
 
 # Open the png device for plotting, type = "cairo" has been added to match the plot with example
-png(file = "./plot3.png", width = 480, height = 480, units = "px", bg = "white", type="cairo")
+# The image file in rdpeng's github (from which fork was done) has transparent background, so adding transparent background
+png(file = "./plot3.png", width = 480, height = 480, units = "px", type="cairo", bg = "transparent")
 
 # Plot
 plot(datetime,dataset$Sub_metering_1, type = "l", xlab = "", ylab = "Energy sub metering", main = "")
